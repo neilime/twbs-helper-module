f36d760bef16d02e10d66404e1e74089c306ba5d
neilime <neilime@pc-neilime>
2017-01-30 18:54:55 +0100

    Create gh-page

--- tests/config/application-config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-    'modules' => array('Zend\Router', 'TwbsHelper'),
-    'module_listener_options' => array(
-        'config_glob_paths' => array(__DIR__ . '/module-config.php'),
-        'module_paths' => array('vendor'),
-    ),
-);
