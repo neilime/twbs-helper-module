f3c500c53152ea09f838ccfd81d527d20b7ced4c
Neilime <escalle.emilien@gmail.com>
2019-08-15 20:46:47 +0200

    Add test for 'Custom forms' doc

---
+++ src/TwbsHelper/Form/View/Helper/MultiCheckboxTrait.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace TwbsHelper\Form\View\Helper;
+
+trait MultiCheckboxTrait
+{
+    use \TwbsHelper\View\Helper\ClassAttributeTrait;
+    use \TwbsHelper\View\Helper\HtmlTrait;
+
+    /**
+     * Render a form <input type="radio"> element from the provided $oElement
+     *
+     * @param \Zend\Form\ElementInterface $oElement
+     * @return string
+     */
+    public function render(\Zend\Form\ElementInterface $oElement): string
+    {
+
+        $bIsCustom = $oElement->getOption('custom');
+
+        $this->setClassesToElement(
+            $oElement,
+            [$bIsCustom ? 'custom-control-input' : 'form-check-input'],
+            ['form-control']
+        );
+
+        // Handle label attributes for options
+        $aValueOptions = $oElement->getValueOptions();
+        foreach ($aValueOptions as &$aOption) {
+            if (!is_array($aOption)) {
+                continue;
+            }
+
+            if (empty($aOption['label'])) {
+                $aOption['attributes'] = $this->setClassesToAttributes(
+                    $aOption['attributes'] ?? [],
+                    ['position-static', 'form-check-input']
+                );
+            }
+
+            if (isset($aOption['attributes']['id'])) {
+                $aOption['label_attributes'] = \Zend\Stdlib\ArrayUtils::merge(
+                    $aOption['label_attributes'] ?? [],
+                    [
+                        'for' => $aOption['attributes']['id'],
+                    ]
+                );
+            }
+        }
+        $oElement->setValueOptions($aValueOptions);
+
+        $sOriginalSeparator = $this->getSeparator();
+        $sTmpSeparator = '[SEPARATOR]';
+        $this->setSeparator($sTmpSeparator);
+
+        $aOriginalLabelAttributes = $this->labelAttributes;
+        $this->labelAttributes = ['class' => $bIsCustom ? 'custom-control-label' : 'form-check-label'];
+
+        $sTmpContent = parent::render($oElement);
+
+        // Restore default params
+        $this->setSeparator($sOriginalSeparator);
+        $this->labelAttributes = $aOriginalLabelAttributes;
+
+        $sContent = '';
+
+        $aGroupClasses = $bIsCustom ? ['custom-control', 'custom-radio'] : ['form-check'];
+
+        if ($oElement->getOption('layout') === \TwbsHelper\Form\View\Helper\Form::LAYOUT_INLINE) {
+            $aGroupClasses[] = $bIsCustom ? 'custom-control-inline' : 'form-check-inline';
+        }
+
+        foreach (explode($sTmpSeparator, $sTmpContent) as $sOptionContent) {
+            // Retrieve input content
+            if (preg_match('/(<label[^>]*>.*)(<input[^>]+>)(.*<\/label[^>]*>)/', $sOptionContent, $aMatches)) {
+                $sLabelContent = $aMatches[1] . $aMatches[3];
+                if (preg_match('/<label[^>]*>\s*<\/label[^>]*>/', $sLabelContent)) {
+                    $sLabelContent = '';
+                }
+
+                $sContent .= ($sContent ? PHP_EOL : '') . $this->htmlElement(
+                    'div',
+                    $this->setClassesToAttributes([], $aGroupClasses),
+                    $aMatches[2] . ($sLabelContent ? PHP_EOL . $sLabelContent : '')
+                );
+            }
+        }
+        return $sContent;
+    }
+}
--- tests/TestSuite/Documentation/Components/Forms/CustomForms.php
+++ tests/TestSuite/Documentation/Components/Forms/CustomForms.php
@@ -16,8 +16,8 @@ return [
                         $oFactory = new \Zend\Form\Factory();
 
                         echo $oView->formRow($oFactory->create([
-                            'type' => 'checkbox',
                             'name' => 'custom_checkbox',
+                            'type' => 'checkbox',
                             'options' => [
                                 'label' => 'Check this custom checkbox',
                                 'use_hidden_element' => false,
@@ -30,10 +30,51 @@ return [
                         ]));
                     },
                     'expected' => '<div class="custom-checkbox&#x20;custom-control">' . PHP_EOL .
-                        '    <input type="checkbox" name="custom_checkbox" id="customCheck1" '.
+                        '    <input type="checkbox" name="custom_checkbox" id="customCheck1" ' .
                         'class="custom-control-input" value="1">' . PHP_EOL .
                         '    <label class="custom-control-label" for="customCheck1">' .
                         'Check this custom checkbox' .
+                        '</label>' . PHP_EOL .
+                        '</div>',
+                ],
+                [
+                    'title' => 'Radios',
+                    'url' => '%bootstrap-url%/components/forms/#checkboxes',
+                    'rendering' => function (\Zend\View\Renderer\PhpRenderer $oView) {
+                        $oFactory = new \Zend\Form\Factory();
+
+                        echo $oView->formRow($oFactory->create([
+                            'name' => 'customRadio',
+                            'type' => 'radio',
+                            'options' => [
+                                'custom' => true,
+                                'value_options' => [
+                                    [
+                                        'label' => 'Toggle this custom radio',
+                                        'value' => '1',
+                                        'attributes' => ['id' => 'customRadio1'],
+                                    ],
+                                    [
+                                        'label' => 'Or toggle this other custom radio',
+                                        'value' => '2',
+                                        'attributes' => ['id' => 'customRadio2'],
+                                    ],
+                                ],
+                            ],
+                        ]));
+                    },
+                    'expected' => '<div class="custom-control&#x20;custom-radio">' . PHP_EOL .
+                        '    <input type="radio" name="customRadio" class="custom-control-input" ' .
+                        'id="customRadio1" value="1">' . PHP_EOL .
+                        '    <label class="custom-control-label" for="customRadio1">' .
+                        'Toggle this custom radio' .
+                        '</label>' . PHP_EOL .
+                        '</div>' . PHP_EOL .
+                        '<div class="custom-control&#x20;custom-radio">' . PHP_EOL .
+                        '    <input type="radio" name="customRadio" class="custom-control-input" ' .
+                        'id="customRadio2" value="2">' . PHP_EOL .
+                        '    <label class="custom-control-label" for="customRadio2">' .
+                        'Or toggle this other custom radio' .
                         '</label>' . PHP_EOL .
                         '</div>',
                 ],
