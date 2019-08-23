8b3238e80bea7d67fbed5939fc0c39992cd267dc
Neilime <escalle.emilien@gmail.com>
2019-08-23 14:36:46 +0200

    Add tests for Input group doc

--- tests/TestSuite/Documentation/Components/InputGroup.php
+++ tests/TestSuite/Documentation/Components/InputGroup.php
@@ -39,7 +39,36 @@ return [
                         'aria-label' => 'Recipient\'s username',
                         'aria-describedby' => 'basic-addon2',
                     ],
-                ]));
+                ])) . PHP_EOL;
+
+                echo $oView->formRow($oFactory->create([
+                    'name' => 'url',
+                    'type' => 'text',
+                    'options' => [
+                        'label' => 'Your vanity URL',
+                        'form_group' => false,
+                        'input_group_class' => 'mb-3',
+                        'add_on_prepend' => 'https://example.com/users/',
+                    ],
+                    'attributes' => [
+                        'id' => 'basic-url',
+                        'aria-describedby' => 'basic-addon3',
+                    ],
+                ])) . PHP_EOL;
+
+                echo $oView->formRow($oFactory->create([
+                    'name' => 'amount',
+                    'type' => 'text',
+                    'options' => [
+                        'form_group' => false,
+                        'input_group_class' => 'mb-3',
+                        'add_on_prepend' => '$',
+                        'add_on_append' => '.00',
+                    ],
+                    'attributes' => [
+                        'aria-label' => 'Amount (to the nearest dollar)',
+                    ],
+                ])) . PHP_EOL;
             },
             'expected' => '<div class="input-group&#x20;mb-3">' . PHP_EOL .
                 '    <div class="input-group-prepend">' . PHP_EOL .
@@ -51,7 +80,7 @@ return [
                 'aria-describedby="basic-addon1" class="form-control" value="">' . PHP_EOL .
                 '</div>' . PHP_EOL .
                 '<div class="input-group&#x20;mb-3">' . PHP_EOL .
-                '    <input type="text" name="recipient_username" placeholder="Recipient&#x27;s&#x20;username" '.
+                '    <input type="text" name="recipient_username" placeholder="Recipient&#x27;s&#x20;username" ' .
                 'aria-label="Recipient&#x27;s&#x20;username" ' .
                 'aria-describedby="basic-addon2" class="form-control" value="">' . PHP_EOL .
                 '    <div class="input-group-append">' . PHP_EOL .
@@ -59,7 +88,32 @@ return [
                 '            @example.com' . PHP_EOL .
                 '        </div>' . PHP_EOL .
                 '    </div>' . PHP_EOL .
-                '</div>',
+                '</div>' . PHP_EOL .
+                '<label for="basic-url">Your vanity URL</label>' . PHP_EOL .
+                '<div class="input-group&#x20;mb-3">' . PHP_EOL .
+                '    <div class="input-group-prepend">' . PHP_EOL .
+                '        <div id="basic-addon3" class="input-group-text">' . PHP_EOL .
+                '            https://example.com/users/' . PHP_EOL .
+                '        </div>' . PHP_EOL .
+                '    </div>' . PHP_EOL .
+                '    <input type="text" name="url" id="basic-url" aria-describedby="basic-addon3" ' .
+                'class="form-control" value="">' . PHP_EOL .
+                '</div>' . PHP_EOL.
+                '<div class="input-group&#x20;mb-3">' . PHP_EOL .
+                '    <div class="input-group-prepend">' . PHP_EOL .
+                '        <div class="input-group-text">' . PHP_EOL .
+                '            $' . PHP_EOL .
+                '        </div>' . PHP_EOL .
+                '    </div>' . PHP_EOL .
+                '    <input type="text" name="amount" '.
+                'aria-label="Amount&#x20;&#x28;to&#x20;the&#x20;nearest&#x20;dollar&#x29;" ' .
+                'class="form-control" value="">' . PHP_EOL .
+                '    <div class="input-group-append">' . PHP_EOL .
+                '        <div class="input-group-text">' . PHP_EOL .
+                '            .00' . PHP_EOL .
+                '        </div>' . PHP_EOL .
+                '    </div>' . PHP_EOL .
+                '</div>' . PHP_EOL,
         ],
     ],
 ];
