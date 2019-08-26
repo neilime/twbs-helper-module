0fd5e9eaff858e448c80614290b661c525f29836
Neilime <escalle.emilien@gmail.com>
2019-08-26 22:22:35 +0200

    Add tests for Nav doc

--- tests/TestSuite/Documentation/Components/Navs.php
+++ tests/TestSuite/Documentation/Components/Navs.php
@@ -151,9 +151,37 @@ return [
                         '    <a class="nav-link&#x20;active" href="&#x23;">Active</a>' . PHP_EOL .
                         '    <a class="nav-link" href="&#x23;">Link</a>' . PHP_EOL .
                         '    <a class="nav-link" href="&#x23;">Link</a>' . PHP_EOL .
-                        '    <a class="nav-link&#x20;disabled" href="&#x23;" tabindex="-1" aria-disabled="true">'.
+                        '    <a class="nav-link&#x20;disabled" href="&#x23;" tabindex="-1" aria-disabled="true">' .
                         'Disabled</a>' . PHP_EOL .
                         '</nav>',
+                ],
+                [
+                    'title' => 'Base nav',
+                    'url' => '%bootstrap-url%/components/navs/#base-nav',
+                    'rendering' => function (\Zend\View\Renderer\PhpRenderer $oView) {
+                        echo $oView->navigation()->menu()->renderMenu(new \Zend\Navigation\Navigation([
+                            ['label' => 'Active', 'uri' => '#', 'active' => true,],
+                            ['label' => 'Link', 'uri' => '#',],
+                            ['label' => 'Link', 'uri' => '#',],
+                            ['label' => 'Disabled', 'uri' => '#', 'visible' => false,],
+                        ]), ['pills' => true]);
+                    },
+                    'expected' => '<ul class="nav&#x20;nav-pills">' . PHP_EOL .
+                        '    <li class="&#x20;nav-item">' . PHP_EOL .
+                        '        <a class="nav-link&#x20;active" href="&#x23;">Active</a>' . PHP_EOL .
+                        '    </li>' . PHP_EOL .
+                        '    <li class="nav-item">' . PHP_EOL .
+                        '        <a class="nav-link" href="&#x23;">Link</a>' . PHP_EOL .
+                        '    </li>' . PHP_EOL .
+                        '    <li class="nav-item">' . PHP_EOL .
+                        '        <a class="nav-link" href="&#x23;">Link</a>' . PHP_EOL .
+                        '    </li>' . PHP_EOL .
+                        '    <li class="nav-item">' . PHP_EOL .
+                        '        <a class="nav-link&#x20;disabled" href="&#x23;" tabindex="-1" aria-disabled="true">' .
+                        'Disabled' .
+                        '</a>' . PHP_EOL .
+                        '    </li>' . PHP_EOL .
+                        '</ul>',
                 ],
             ],
         ],
