cd0c139fd2ac6f945031a0f1a2912d0ffa1ffc3f
Emilien Escalle <emilien.escalle@escemi.com>
2021-10-01 18:36:23 +0200

    fix(View): add tests for indentation issue reproduction

--- tests/TestSuite/TwbsHelper/View/Helper/HtmlTraitTest.php
+++ tests/TestSuite/TwbsHelper/View/Helper/HtmlTraitTest.php
@@ -25,4 +25,51 @@ class HtmlTraitTest extends \PHPUnit\Framework\TestCase
             $sResult
         );
     }
+
+    public function testAddProperIndentationForNestedFieldset()
+    {
+        $sContent = '<form method="post" name="formEditor" class=" layout-form-padding" role="form" id="formEditor">
+    <fieldset disabled="disabled" class="form-group">
+        <div class="row">
+            <legend class="col-form-label">...</legend>
+            <fieldset class="w-100">
+                <div class="row">
+                    <legend class="col-form-label">...</legend>
+                    <fieldset class="w-100">
+                        <div class="row">
+                            <legend class="col-form-label">...</legend>
+                            <div class="col-sm-12">
+                                <label for="...">...</label>
+                                <input type="text" name="..." list="..." class="form-control" ' .
+            'value="frei" autocomplete="off">
+                            </div>
+                        </div>
+                    </fieldset>
+                    <fieldset class="w-100">
+                        <div class="row">
+                            <legend class="col-form-label">...</legend>
+                            <div class="col-sm-12">
+                                <label for="...">....</label>
+                                <input type="text" name="..." list="..." class="form-control" ' .
+            'value="frei" autocomplete="off">
+                            </div>
+                        </div>
+                    </fieldset>
+                </div>
+            </fieldset>
+        </div>
+    </fieldset>
+</form>';
+
+        $sResult = $this->trait->addProperIndentation($sContent, true);
+
+        $sExpectedContent = PHP_EOL . implode(PHP_EOL, array_map(function ($sLine) {
+            return '    ' . $sLine;
+        }, explode(PHP_EOL, $sContent))) . PHP_EOL;
+
+        $this->assertSame(
+            $sExpectedContent,
+            $sResult
+        );
+    }
 }
