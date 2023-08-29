68f755dd4f5b2f66a4333b0e50c282f4addd8e89
Emilien Escalle <emilien.escalle@escemi.com>
2023-08-29 12:18:17 +0200

    test: add more tests for Gutter class helper

--- tests/TestSuite/TwbsHelper/View/Helper/HtmlAttributes/HtmlClass/Helper/GutterTest.php
+++ tests/TestSuite/TwbsHelper/View/Helper/HtmlAttributes/HtmlClass/Helper/GutterTest.php
@@ -19,4 +19,46 @@ class GutterTest extends \PHPUnit\Framework\TestCase
     {
         $this->assertEquals(['g-0'], $this->helper->getClassesFromOption(0));
     }
+
+    public function testGetClassesFromOptionWithInvalidTypeOption()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            '"gutter" option expects an integer, a string or an array, "stdClass" given'
+        );
+
+        $this->helper->getClassesFromOption(new \stdClass());
+    }
+
+    public function testGetClassesFromOptionWithInvalidArrayOption()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            '"gutter" array option does accept given keys "invalid", it only accepts "horizontal, vertical"'
+        );
+
+        $this->helper->getClassesFromOption(['invalid' => 'option']);
+    }
+
+    public function testGetClassesFromOptionWithInvalidStringOption()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            '"gutter" option "invalid" is not supported. Expects a number between 0 and 5; ' .
+                'or a sized (xs,sm,md,lg,xl,xxl) numbered value. Example: "sm-5"'
+        );
+
+        $this->helper->getClassesFromOption('invalid');
+    }
+
+    public function testGetClassesFromOptionWithInvalidIntOption()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            '"gutter" option "6" is not supported. Expects a number between 0 and 5; ' .
+                'or a sized (xs,sm,md,lg,xl,xxl) numbered value. Example: "sm-5"'
+        );
+
+        $this->helper->getClassesFromOption(6);
+    }
 }
